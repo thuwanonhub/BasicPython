ce02349746fb5ab9a8eae3b4713176d4192b8d8d
thuwanonhub <109516409+thuwanonhub@users.noreply.github.com>
2024-10-04 23:06:54 +0700

    Upload test 1

---
+++ Ep4-3_Basic_input.py
@@ -0,0 +1,16 @@
+'''
+for i in range(4):
+
+    print(f'Hello Big {i+1} ')
+    
+    print(i)
+'''
+
+friend =  []
+
+for i in range(5):
+    data = input('Who is your friend?: ')
+    friend.append(data)
+
+print(friend)
+
